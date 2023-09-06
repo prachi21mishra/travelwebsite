fb96fb31e24ff8d1bbcea79f6d30d4fb3e399594
prachi mishra <138745084+prachi21mishra@users.noreply.github.com>
2023-09-06 11:34:42 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/homestyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    
+</head>
+<body>
+    
+    <nav>
+        <input type="checkbox" id="check" name="" value="">
+        <label for="check" id= "hambtn"><i class="fa fa-bars"></i></label>
+        <label class="logo">RUDRA TRAVELS</label>
+        <UL>
+            <li><a href="hpgfinal.php">home</a></li>
+               <li> <a href="aboutus.php">about</a></li>
+                <li><a href="package.php">package</a></li>
+               <li> <a href="form.php">book </a></LI>
+        </UL>
+    </nav>
+    
+    <div class="heading">
+        <h1>About Us</h1>
+        <p> Lorem ipsum dolor sit amet </p>
+    </div>
+    <section class="about-us">
+    <img src="images/i3.jpg">
+    <div class="content">
+        <h2>xyz pqr vstv jhhkkl</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere repudiandae praesentium amet?Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit!
+
+        </p>
+    <button class="read-more-btn">Read More</button>
+    </div>
+    </section>
+</body>
+</html>
